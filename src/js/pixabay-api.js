b02abd5aa6da0671deb3ff96520f9b83d70ab2e3
Nastya <nastyusha3@gmail.com>
2024-09-05 15:13:28 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,19 +2,25 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://pixabay.com/api';
 
-export const fetchPhotos = (searchedQuery, page) => {
+export const fetchPhotos = async (searchedQuery, page) => {
+  try {
     const axiosOptions = {
-        params: {
+      params: {
         key: '45695124-2521d690be74d3f32382c65dc',
         q: searchedQuery,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
         page: page,
-        per_page: 15
-    },
-};
+        per_page: 15,
+      },
+    };
 
-    return axios.get(`/`, axiosOptions);
+    const response = await axios.get('/', axiosOptions);
 
+    return response;
+  } catch (error) {
+    console.error('Error fetching photos:', error);
+    throw error;
+  }
 };
